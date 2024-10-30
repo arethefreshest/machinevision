1ee77420d0e8589cfba3b22ef4633b39647a8b60
arethefreshest <doffen.are@gmail.com>
2024-10-30 13:09:02 +0100

    Added environment setup through docker and documentation on how to run app in README.md

--- App/App.py
+++ App/App.py
@@ -63,4 +63,4 @@ fastapi_app.mount("/", WSGIMiddleware(dash_app.server))
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run("App:fastapi_app", host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run("App:fastapi_app", host="127.0.0.1", port=8000, reload=True)
